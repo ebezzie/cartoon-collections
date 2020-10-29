92f94a57c12e3dc71f2250f60240444def58f908
Ebony <ebony.furr@yahoo.com>
2020-10-28 19:13:59 -0500

    commit tree 676bdaf061cd69fa5605981619f8995dc0567ea2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) # code an argument here
+    place = 1
+    dwarves.each do |name|
+      puts "#{place}. #{name}"
+      place += 1
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)# code an argument here
+  planeteer_calls.collect { |x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  if array.size > 4
+    false
+  else
+    true
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|name| cheese_types.include?(name)}
 end
